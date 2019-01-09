f132d334a21870ba2f51d9ceedc331600958b4a4
Liam Gooch <liam@purplecorridor.io>
2019-01-09 01:20:44 +0000

    Added requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aws-tools',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/lgooch/aws-tools',
+    license='MIT',
+    author='Liam Gooch',
+    author_email='liam@purplecorridor.io',
+    description='A tool to delete the default VPC\'s in an account'
+)
